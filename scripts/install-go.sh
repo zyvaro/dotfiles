611cdb01364e3ecccb708df91dd8eed80ca46996
dedNewGuy <aminkafrinewgen@gmail.com>
2025-07-25 11:57:13 +0800

    add more scripts

---
+++ scripts/install-go.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -xe
+
+GOVER=$(curl -s https://go.dev/dl/ | grep -o '<span>go.*linux-amd64.tar.gz</span>' | sed 's/<span>\(.*\)<\/span>/\1/' | sed 's/^[[:space:]]*//')
+wget https://go.dev/dl/$GOVER
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+
+rm go1.24.5.linux-amd64.tar.gz
---
+++ scripts/install-nodejs-ubuntu.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -xe
+curl -fsSL https://deb.nodesource.com/setup_23.x -o nodesource_setup.sh
+sudo -E bash nodesource_setup.sh
+sudo apt install -y nodejs
+
+mkdir -p ~/.npm-global/
+echo "export PATH=\$PATH:~/.npm-global/bin" >> ~/.bashrc
